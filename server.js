74c25ad844208d2858284d69dc93a6a2b4f3c46c
Luci <texasjezzy87@gmail.com>
2020-11-17 22:38:37 -0600

    initial app

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
 useFindAndModify: false});
 
 
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
